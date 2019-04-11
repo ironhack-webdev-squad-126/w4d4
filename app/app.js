f210d7d2c9ba41dd83144bf78b9c1514a005c733
mjarraya <mjarraya@student.42.fr>
2019-04-11 09:58:55 +0200

    simple dynamic templating features

--- app/app.js
+++ app/app.js
@@ -6,8 +6,26 @@ app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   res.render('index', {
-    name: 'Sylvie'
+    name: 'Sylvie',
+    bool: Math.round(Math.random())
   });
+});
+
+app.get('/games', (req, res) => {
+  const paramObject = {
+    gamesList: ['Counter-Strike', 'BF3', 'Sims', 'Street Fighter'].map(el =>
+      el.toUpperCase()
+    ),
+    consolesObj: {
+      Sony: 'PS4',
+      Nintendo: 'Switch',
+      Microsoft: 'Xbox One'
+    },
+    country: {
+      name: 'Japan'
+    }
+  };
+  res.render('games', paramObject);
 });
 
 app.listen(3000);
